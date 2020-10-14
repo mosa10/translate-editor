d7fce71ae89a14aa5725194edacf225b9fedebb1
mosa10 <mosa77777@gmail.com>
2020-10-14 20:22:40 +0900

    update: タグ変換

--- public/js/editor.js
+++ public/js/editor.js
@@ -25,7 +25,7 @@ let editor = {};
         $.ajax({
             'url': '/editor/1/sentence',
             'success': function( data ){
-                console.log(data);
+                //console.log(data);
             }
         });
     };
@@ -37,15 +37,27 @@ let editor = {};
     let mousedown = false;
 
     $(document).on('keyup', function( event ){
-        console.log('document keyup');
+        //console.log('document keyup');
 
         switch ( event.keyCode ){
 
         }
     });
 
+    $('.input').on('copy', function( event ){
+        return false;
+    });
+
+    $('.input').on('paste', function( event ){
+        return false;
+    });
+
+    $('.input').on('contextmenu', function( event ){
+        return false;
+    });
+
     $('.input').on('keydown', function( event ){
-        console.log('keydown', event.keyCode, composition);
+        //console.log('keydown', event.keyCode, composition);
 
         switch ( event.keyCode ){
             // 左矢印
@@ -53,32 +65,63 @@ let editor = {};
                 if ( ! composition ){
                     if ( event.shiftKey ){
                         current.prev().toggleClass('selection');
+                    } else {
+                        clearSelect();
                     }
 
                     moveCursor(current.prev(), true);
                 }
+                return false;
+            // 上矢印
+            case 38:
+                clearSelect();
+
+                var moveElm = guessMoveElm(current, true);
+                if ( event.shiftKey ){
+                    select($('.txt').index($('.txt.selection-start')), $('.txt').index(moveElm));
+                }
+                moveCursor(moveElm, true);
                 return false;
             // 右矢印
             case 39:
                 if ( ! composition ){
                     if ( event.shiftKey ){
                         current.toggleClass('selection');
+                    } else {
+                        clearSelect();
                     }
                     
                     moveCursor(current.next(), true);
                 }
+                return false;
+            // 下矢印
+            case 40:
+                clearSelect();
+                var moveElm = guessMoveElm(current, false);
+                if ( event.shiftKey ){
+                    select($('.txt').index($('.txt.selection-start')), $('.txt').index(moveElm));
+                }
+                moveCursor(moveElm, true);
                 return false;
         }
     });
 
     $('.input').on('keyup', function( event ){
         console.log('keyup', event.keyCode, composition);
+
+        event.preventDefault();
+        event.stopPropagation();
+        
         let isComposing = false;
 
         switch ( event.keyCode ){
             // back space
             case 8:
-                removeTxt(current.prev());
+                if ( $('.txt.selection').length > 0 ){
+                    $('.txt.selection').remove();
+                } else {
+                    removeTxt(current.prev());
+                }
                 return false;
             // Enter
             case 13:
@@ -91,8 +134,40 @@ let editor = {};
                 return false;
             // delete
             case 46:
-                removeTxt(current);
+                if ( $('.txt.selection').length > 0 ){
+                    $('.txt.selection').remove();
+                } else {
+                    removeTxt(current);
+                }
                 return false;
+            // a
+            case 65:
+                if ( event.ctrlKey ){
+                    selectAll();
+                    return false;
+                }
+                break;
+            // c
+            case 67:
+                if ( event.ctrlKey ){
+                    copyText();
+                    return false;
+                }
+                break;
+            // v
+            case 86:
+                if ( event.ctrlKey ){
+                    pasteText();
+                    return false;
+                }
+                break;
+            // x
+            case 88:
+                if ( event.ctrlKey ){
+                    cutText();
+                    return false;
+                }
+                break;
             case 229:
                 //isComposing = true;
                 break;
@@ -100,7 +175,13 @@ let editor = {};
 
         let val = $(this).val();
 
-        console.log(val);
+        if ( val.length == 0 ){
+            return false;
+        }
+
+        //console.log(val);
+
+        $('.txt.selection').remove();
         
         if ( composition ){
             $('.pre').remove();
@@ -118,8 +199,10 @@ let editor = {};
             if ( elm.length > 0 ){
                 moveCursor(elm, false);
             } else {
-                //moveCursor(current, false);
+                moveCursor(current, false);
             }
+
+            moveInput();
         } else {
             current.before($('<span>', {
                 'class': 'txt'
@@ -129,16 +212,24 @@ let editor = {};
 
             moveCursor(current, true);
         }
+
+        return false;
     });
 
     $('.input').on('compositionstart', function(){
-        console.log('compositionstart');
+        //console.log('compositionstart');
         composition = true;
+
+        $(this).css('width', '1000px');
+
+        moveInput();
     });
 
     $('.input').on('compositionend', function(){
-        console.log('compositionend');
+        //console.log('compositionend');
         composition = false;
+
+        $(this).css('width', '0');
 
         $('.pre').removeClass('pre').addClass('txt');
 
@@ -146,14 +237,47 @@ let editor = {};
     });
 
     $('.input').on('blur', function(){
-        console.log('blur');
+        //console.log('blur');
         $('.pre').removeClass('pre').addClass('txt');
 
         $(this).val('');
     });
 
+    $(document).on('click', '.txt', function( event ){
+        // ダブルクリック
+        if ( event.detail === 2 ){
+            let indexStart = 0;
+            let indexEnd = $('.txt').length - 1;
+    
+            let indexTarget = $('.txt').index(this);
+    
+            $('.txt').each(function( index ){
+                if ( $.trim($(this).text()).length === 0 ){
+                    if ( index < indexTarget ) {
+                        indexStart = index;
+                    } else {
+                        indexEnd = index;
+    
+                        return false;
+                    }
+                }
+            });
+    
+            $('.txt').removeClass('selection');
+            $('.txt').slice(indexStart, indexEnd).addClass('selection');
+        }
+        // トリプルクリック
+        else if ( event.detail === 3 ){
+            $('.txt').removeClass('selection');
+            $('.txt').addClass('selection');
+        }
+    });
+
     $(document).on('mousedown', '.txt', function( event ){
+        //console.log('mousedown');
+
         event.preventDefault();
+        event.stopPropagation();
 
         mousedown = true;
 
@@ -167,34 +291,157 @@ let editor = {};
     });
 
     $(document).on('mouseup', function(){
+        //console.log('mouseup');
+
         mousedown = false;
     });
 
-    $(document).on('mouseover', '.txt', function(){
+    $(document).on('mouseleave', '.txt', function(){
+        //console.log('mouseleave', mousedown, $(this).text());
+
         if ( mousedown ){
-            if ( $(this).hasClass('selection-start') ){
-                return false;
-            }
+            let indexStart = $('.txt').index($('.txt.selection-start'));
+            let indexEnd = $('.txt').index(this);
 
-            $('*').removeClass('selection');
+            select(indexStart, indexEnd);
 
-            let that = this;
-            let isStart = false;
-
-            $('.txt').each(function(){
-                if ( $(this).hasClass('selection-start') || this == that ){
-                    isStart = ! isStart;
-                }
-
-                if ( isStart ){
-                    $(this).addClass('selection');
-                }
-            });
+            moveCursor(indexStart < indexEnd ? $(this).next() : this, true);
         }
     });
 
+    const select = function( indexStart, indexEnd ){
+        console.log('start', indexStart, indexEnd);
+
+        $('.txt').removeClass('selection');
+
+        let isStart = false;
+        let isLast = false;
+
+        let isBack = indexStart < indexEnd;
+
+        // 前から走査
+        $('.txt').each(function( index ){
+            if ( index == indexStart ){
+                if ( isBack ){
+                    isStart = true;
+                } else {
+                    isLast = true;
+                }
+            }
+            if ( index == indexEnd ){
+                if ( isBack ){
+                    isLast = true;
+                } else {
+                    isStart = true;
+                }
+            }
+
+            if ( isStart ){
+                $(this).addClass('selection');
+            }
+
+            if ( isLast ){
+                return false;
+            }
+        });
+    };
+
+    const selectAll = function(){
+        $('.txt').removeClass('selection');
+        $('.txt').addClass('selection');
+    };
+
+    const clearSelect = function(){
+        $('.txt').removeClass('selection');
+    };
+
+    const copyText = function(){
+        //console.log('copyText');
+
+        let text = '';
+
+        $('.txt.selection').each(function(){
+            text += $(this).text();
+        });
+
+        console.log(text);
+
+        $('.copy').val(text);
+
+        $('.copy').select();
+        document.execCommand('copy');
+    };
+
+    const cutText = function(){
+        //console.log('cutText');
+
+        let text = '';
+
+        $('.txt.selection').each(function(){
+            text += $(this).text();
+        });
+
+        $('.txt.selection').remove();
+
+        console.log(text);
+
+        $('.copy').val(text);
+    };
+
+    const pasteText = function(){
+        //console.log('pasteText');
+
+        let text = $('.copy').val();
+        console.log(text);
+
+        text.split('').forEach( txt => {
+            current.before($('<span>', {
+                'class': 'txt'
+            }).text(txt));
+        });
+
+        $('.txt.selection').remove();
+    };
+
+    const guessMoveElm = function( elm, isUp ){
+        let target = elm;
+        let diffTop = Number.MAX_SAFE_INTEGER;
+        let diffLeft = Number.MAX_SAFE_INTEGER;
+
+        let top = $(elm).offset().top;
+        let left = $(elm).offset().left;
+
+        
+        $('.txt').each(function(){
+            let targetTop = $(this).offset().top;
+            let targetLeft = $(this).offset().left;
+            
+            if ( (isUp && targetTop < top) || (! isUp && targetTop > top) ){
+                let tmpTop = Math.abs(top - targetTop);
+                let tmpLeft = Math.abs(left - targetLeft);
+                
+                if ( tmpTop <= diffTop ){
+                    if ( tmpTop != diffTop ){
+                        diffLeft = Number.MAX_SAFE_INTEGER;
+                    }
+
+                    diffTop = tmpTop;
+
+                    if ( tmpLeft <= diffLeft ){
+                        target = this;
+                        diffLeft = tmpLeft;
+                    }
+                }
+            }
+        });
+        
+        console.log(top, left, $(target).offset().top, $(target).offset().left, diffTop, diffLeft);
+
+        return target;
+    };
+
     const moveCursor = function( elm, is ){
-        console.log('moveCursor', elm, is);
+        //console.log('moveCursor', elm, is);
 
         if ( $(elm).length === 0 ){
             return false;
@@ -204,30 +451,45 @@ let editor = {};
             current = $(elm);
         }
 
-        let top = $(elm).offset().top;
-        let left = $(elm).offset().left;
 
-        console.log(top, left);
-        console.log(current.text());
+        let targetClass = is ? '.txt' : '.pre';
 
-        if ( top == 0 ){
-            console.log(current.text());
-        }
-
-        $(is ? '.cursor' : '.cursor-red').css({
-            'top': top,
-            'left': left
-        });
+        $(targetClass).removeClass('caret');
+        $(elm).addClass('caret');
 
         $('.input').focus();
     };
 
-    const removeTxt = function( elm ){
-        if ( $(elm).length === 0 ){
-            return false;
+    const moveInput = function(){
+        let top = 0;
+        let left = 0;
+
+        if ( $('.pre').length == 0 ){
+            top = $(current).offset().top + $(current).outerHeight();
+            left = $(current).offset().left;
+        } else {
+            $('.pre').each(function(){
+                top = Math.max(top, $(this).offset().top + $(this).outerHeight());
+            });
+
+            left = $('.pre').first().offset().left;
         }
 
-        if ( $(elm).hasClass('txt-end') ){
+        console.log(top, left);
+        //console.log(current.text());
+
+        if ( top == 0 ){
+            //console.log(current.text());
+        }
+
+        $('.cursor').css({
+            'top': top,
+            'left': left
+        });
+    };
+
+    const removeTxt = function( elm ){
+        if ( $(elm).length === 0 ){
             return false;
         }
 
--- public/js/editor.view.js
+++ public/js/editor.view.js
@@ -1,7 +1,121 @@
 class EditorView {
+    editorUtil = new EditorUtil;
+    
     convertHTML(){
-        $('.text').each(function(){
+        $('.text').each((index, element) => {
+            let that = this;
+            let text = $(element).text();
 
+            $(element).empty();
+
+            // タグ検索
+            let match = text.match(/{g:[0-9]+}|{\/g}/g);
+
+            if ( match && match.length > 0 ){
+                let listText = [text];
+                
+                var tmp = [];
+
+                tmp = this.sss(listText, match, 0);
+                tmp = this.ttt(tmp);
+                tmp = tmp.filter(v => v);
+                
+                tmp.forEach( t => {
+                    let isTag = false;
+
+                    match.forEach( m => {
+                        if ( m == t ){
+                            isTag = true;
+                            return false;
+                        }
+                    });
+
+                    if ( isTag ){
+                        var tag = this.converTag(t);
+                        $(element).append($(tag));
+                    } else {
+                        t.split('').forEach( t2 => {
+                            $(element).append($('<span>', {
+                                'class': 'txt'
+                            }).text(t2));
+                        });
+                    }
+                });
+            } else {
+                text.split('').forEach( t2 => {
+                    $(element).append($('<span>', {
+                        'class': 'txt'
+                    }).text(t2));
+                });
+            }
+            $(element).append($('<span>', {
+                'class': 'txt txt-end'
+            }));
+       });
+    }
+
+    converTag( str ){
+        str = str.replace(/{g:([0-9])+}/g, '<span class="tag tag-start">$1</span>');
+        str = str.replace(/{\/g}/g, '<span class="tag tag-end"></span>');
+
+        return str;
+    }
+
+    sss( listText, keys, index ){
+        if ( keys.length - 1 < index ){
+            return listText;
+        }
+
+        var tmp = [];
+        var key = keys[index];
+
+        console.log("1", listText);
+
+        /*
+         * ABC<1>DEF<2>GHQ
+         */
+        listText.forEach( text => {
+            console.log("2", text);
+            /*
+             * ABC,DEF<2>GHQ
+             */
+            let split = text.split(key);
+            console.log("3", split);
+
+            if ( split.length > 1 ){
+                var t = [];
+
+                let a = this.sss(split, keys, ++index);
+
+                a.forEach( b => {
+                    t.push(b);
+                    t.push(key);
+                });
+
+                t.pop();
+                tmp.push(t);
+            } else {
+                tmp.push(split[0]);
+            }
+
+            console.log("4", tmp);
         });
+
+        return tmp;
+    }
+
+    ttt( list ){
+        var tmp = [];
+
+        list.forEach( i => {
+            if ( $.isArray(i) ){
+                var tmp2 = this.ttt(i);
+                tmp = tmp.concat(tmp2);
+            } else {
+                tmp.push(i)
+            }
+        });
+
+        return tmp;
     }
 }
